cdb66527254dbd9f9e1f92d8e96b24c843d5fa59
Sven Rademakers <sven.rademakers@gmail.com>
2017-09-14 18:48:37 +0200

    helloTriangle: fix dependancies

--- examples/helloTriangle.cpp
+++ examples/helloTriangle.cpp
@@ -3,7 +3,13 @@
 #include <stdio.h>
 #include <iostream>
 
+#include "WindowGlfw.hpp"
+#include "RendererVulkan.hpp"
+
 int main(void)
 {
+	static sx::WindowGlfw window("Hello Triangle", 800, 600);
+	static sx::RendererVulkan vulcano(window);
+
 	return 0;
 }
