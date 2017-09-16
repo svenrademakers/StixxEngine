a541de3f88afd6166aec00dd8a831984a497f130
Sven Rademakers <sven.rademakers@gmail.com>
2017-09-17 00:43:41 +0200

    Cmake: Shader: fixed freakin cmake quotes mess

--- stixxShaders/CompileGLSL.cmake
+++ stixxShaders/CompileGLSL.cmake
@@ -1,9 +1,7 @@
-set(VULKAN_ROOT "C:/VulkanSDK/1.0.57.0")
+set(VULKAN_ROOT C:/VulkanSDK/1.0.57.0)
 
 foreach(shader ${shaders})
-	execute_process(COMMAND "${VULKAN_ROOT}/Bin/glslangValidator.exe -V ${shader}" RESULT_VARIABLE result)
-	if(NOT result STREQUAL "")
-		MESSAGE(FATAL_ERROR "could not compile ${shader}")
-	endif()
+	SET(cmd ${VULKAN_ROOT}/Bin/glslangValidator.exe -V ${shader})
+	execute_process(COMMAND ${cmd} RESULT_VARIABLE result)
 endforeach()
 
