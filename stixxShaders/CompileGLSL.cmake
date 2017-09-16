bad0989a5b09d24323cc97318a373a469077085f
Sven Rademakers <sven.rademakers@gmail.com>
2017-09-16 23:53:06 +0200

    Cmake: CompileGLSL: compile shader files

--- stixxShaders/CompileGLSL.cmake
+++ stixxShaders/CompileGLSL.cmake
@@ -1,6 +1,9 @@
 set(VULKAN_ROOT "C:/VulkanSDK/1.0.57.0")
 
 foreach(shader ${shaders})
-	execute_process(COMMAND "${VULKAN_ROOT}/Bin/glslangValidator.exe -V ${shader}" OUTPUT_VARIABLE  wat)
-	message(STATUS "${wat}")
+	execute_process(COMMAND "${VULKAN_ROOT}/Bin/glslangValidator.exe -V ${shader}" RESULT_VARIABLE result)
+	if(NOT result STREQUAL "")
+		MESSAGE(FATAL_ERROR "could not compile ${shader}")
+	endif()
 endforeach()
+
