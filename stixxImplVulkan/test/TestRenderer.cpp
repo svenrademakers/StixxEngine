df7a413b7b1b513c2af648efdbbaaa16d911e0c4
Sven Rademakers <sven.rademakers@gmail.com>
2017-09-23 21:54:57 +0200

    micro-test: add framework

---
+++ stixxImplVulkan/test/TestRenderer.cpp
@@ -0,0 +1,14 @@
+#include "gmock/gmock.h"
+
+#include "RendererVulkan.hpp"
+
+class TestRenderer : public ::testing::Test
+{
+protected:
+
+};
+
+TEST_F(TestRenderer, hoi)
+{
+    EXPECT_FALSE(false);
+}
