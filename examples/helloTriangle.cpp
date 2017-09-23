bad787cab6433c81b9a7deea5c29a71f9b99519c
Sven Rademakers <sven.rademakers@gmail.com>
2017-09-23 21:27:29 +0200

    move external libs

--- examples/helloTriangle.cpp
+++ examples/helloTriangle.cpp
@@ -3,23 +3,20 @@
 #include "WindowGlfw.hpp"
 #include "FileSystem.hpp"
 #include "RendererVulkan.hpp"
+#include "MeshLoaderAssimp.hpp"
 
 int main(void)
 {
-    try {
-        static sx::FileSystemStd fileSystem;
-        static sx::WindowGlfw window("Hello Triangle", 800, 600);
-        static sx::RendererVulkan renderer(window, fileSystem.LoadFile("/home/sven/stixx/stixxShaders/vert.spv"), fileSystem.LoadFile("/home/sven/stixx/stixxShaders/frag.spv"));
+    static MeshLoaderAssimp meshLoader("cube.obj");
 
-        while (!window.ShouldClose()) {
-            window.Poll();
-            renderer.Draw();
-        }
-    }
-    catch(std::runtime_error err)
-    {
-        std::cerr << err.what();
+    static sx::FileSystemStd fileSystem;
+    static sx::WindowGlfw window("Hello Triangle", 800, 600);
+    static sx::RendererVulkan renderer(window, fileSystem.LoadFile("/home/sven/stixx/stixxShaders/vert.spv"), fileSystem.LoadFile("/home/sven/stixx/stixxShaders/frag.spv"));
+
+    while (!window.ShouldClose()) {
+        window.Poll();
+        renderer.Draw();
     }
 
-	return 0;
+    return 0;
 }
