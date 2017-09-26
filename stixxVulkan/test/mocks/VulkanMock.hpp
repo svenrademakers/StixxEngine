5feba40aa6d34aa22c6e09f40bedfe3ef27f01d2
Sven Rademakers <sven.rademakers@gmail.com>
2017-09-26 18:41:44 +0200

    park micro testing for rendering component

--- stixxVulkan/test/mocks/VulkanMock.hpp
+++ stixxVulkan/test/mocks/VulkanMock.hpp
@@ -6,9 +6,11 @@ class VulkanMock
 public:
     MOCK_METHOD3(vkCreateInstance, VkResult(const VkInstanceCreateInfo*, const VkAllocationCallbacks*, VkInstance*));
     MOCK_METHOD4(vkCreateBuffer, VkResult(VkDevice, const VkBufferCreateInfo*, const VkAllocationCallbacks*, VkBuffer*));
+    MOCK_METHOD3(vkEnumeratePhysicalDevices, VkResult(VkInstance, uint32_t*, VkPhysicalDevice*));
+    MOCK_METHOD2(vkDestroyInstance, VkResult(VkInstance,const VkAllocationCallbacks*));
 };
 
-static VulkanMock vulkanMock;
+static VulkanMock* vulkanHook = nullptr;
 
 VkResult vkCreateBuffer(
         VkDevice                                    device,
@@ -16,7 +18,7 @@ VkResult vkCreateBuffer(
         const VkAllocationCallbacks*                pAllocator,
         VkBuffer*                                   pBuffer)
 {
-    vulkanMock.vkCreateBuffer(device, pCreateInfo, pAllocator, pBuffer);
+    vulkanHook->vkCreateBuffer(device, pCreateInfo, pAllocator, pBuffer);
 }
 
 VkResult VKAPI_CALL vkCreateInstance(
@@ -24,5 +26,20 @@ VkResult VKAPI_CALL vkCreateInstance(
         const VkAllocationCallbacks*                pAllocator,
         VkInstance*                                 pInstance)
 {
-    vulkanMock.vkCreateInstance(pCreateInfo, pAllocator, pInstance);
+    vulkanHook->vkCreateInstance(pCreateInfo, pAllocator, pInstance);
+}
+
+VkResult vkEnumeratePhysicalDevices(
+        VkInstance                                  instance,
+        uint32_t*                                   pPhysicalDeviceCount,
+        VkPhysicalDevice*                           pPhysicalDevices)
+{
+    vulkanHook->vkEnumeratePhysicalDevices(instance, pPhysicalDeviceCount, pPhysicalDevices);
+}
+
+VKAPI_ATTR void VKAPI_CALL vkDestroyInstance(
+        VkInstance                                  instance,
+        const VkAllocationCallbacks*                pAllocator)
+{
+    vulkanHook->vkDestroyInstance(instance, pAllocator);
 }
