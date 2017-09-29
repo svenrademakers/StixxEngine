2d6206c2155cdd38695c211ed60cf06eaac55c30
Sven Rademakers <sven.rademakers@gmail.com>
2017-09-29 14:14:42 +0200

    InstanceVulkan: fix crash

--- stixxVulkan/src/InstanceVulkan.cpp
+++ stixxVulkan/src/InstanceVulkan.cpp
@@ -15,15 +15,18 @@ namespace sx
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.apiVersion = VK_API_VERSION_1_0;
 
+        auto extension = window.InstanceExtensions();
+
 		VkInstanceCreateInfo InstanceInfo = {};
 		InstanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		InstanceInfo.pApplicationInfo = &appInfo;
-		InstanceInfo.enabledExtensionCount = static_cast<uint32_t>(window.InstanceExtensions().size());
-		InstanceInfo.ppEnabledExtensionNames = window.InstanceExtensions().data();
+		InstanceInfo.enabledExtensionCount = static_cast<uint32_t>(extension.size());
+		InstanceInfo.ppEnabledExtensionNames = extension.data();
 		InstanceInfo.enabledLayerCount = 0;
-        if (vkCreateInstance(&InstanceInfo, nullptr, &handle) != VK_SUCCESS)
+
+		if(vkCreateInstance(&InstanceInfo, nullptr, &handle) != VK_SUCCESS)
 			throw std::runtime_error("failed to create instance.");
-	}
+    }
 
 	InstanceVulkan::~InstanceVulkan()
 	{
